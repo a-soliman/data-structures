4314f8be2310e8ca52d5bf49a1b6e72608d620af
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-05 10:26:51 -0800

    coded a Node constructer function

--- js/main.js
+++ js/main.js
@@ -41,3 +41,9 @@ function LinkedList() {
 	this.head = null;
 	this.tail = null;
 }
+// Node constructor function
+function Node(value, next, prev) {
+	this.value = value;
+	this.next = next;
+	this.prev = prev;
+}
