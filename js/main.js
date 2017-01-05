560505fe32cd212e8e4ec48ecc94e616bd649bb6
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-05 10:22:50 -0800

    intro to constructor function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
 // 	window.location.reload(1);
 // }, 5000)
 //=====================================================================
+					//START
+
+	//== creating constructor function
+// function User(firstName, lastName, age, gender) {
+// 	this.firstName = firstName
+// 	this.lastName = lastName
+// 	this.age = age
+// 	this.gender = gender
+// }
+// var user1 = new User("John", "Smith", 26, "male")
+
+// var user200 = new User("Jill","Gordon", 23, "female")
+
+// console.log(user200)
+
+//============================================================
