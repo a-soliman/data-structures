5a78bdd0674ff971db14f92115d26968a0c7e118
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-07 10:38:57 -0800

    depthFirstTraversal method

--- js/main.js
+++ js/main.js
@@ -245,3 +245,24 @@ BST.prototype.contains = function(value) {
 		}
 	}
 }
+// depthFirstTraversal Method
+BST.prototype.depthFirstTraversal = function(iteratorFunc, order) {
+	if(order === 'pre-order') {
+		iteratorFunc(this.value)
+	}
+	if(this.left) {
+		this.left.depthFirstTraversal(iteratorFunc, order);
+	}
+	if(order === 'in-order') {
+		iteratorFunc(this.value);
+	}
+
+	if(this.right) {
+		this.right.depthFirstTraversal(iteratorFunc, order)
+	}
+
+	if(order === 'post-order') {
+		iteratorFunc(this.value)
+	}
+	
+}
