e711f4f90148cbdd7cb94f4976b5cdfe09f25916
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-09 10:51:04 -0800

    testing the Hash table

--- js/main.js
+++ js/main.js
@@ -423,3 +423,11 @@ HashTable.prototype.retriveAll = function() {
 	}
 	return allNodes;
 }
+ var myHT = new HashTable(30);
+ myHT.insert('Dean', 'dean@gmail.com')
+ myHT.insert('Megan', 'megan@gmail.com')
+ myHT.insert('Dane','dane@yahoo.com')
+ // console.log(myHT.buckets)
+// console.log(myHT.get('Dane'))
+
+console.log(myHT.retriveAll())
